7ac862406770413a01682e5f940e4626e3026124
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2017-11-28 15:35:51 +0100

    src/fasta_comparison.py - creation of a function named counting_letters_frequencies that will count the frequencies of each word in a fasta file - creation of the functions get_frequencies, calcul_dic used in counting_letters_frequencies function

--- fasta_generator/src/fasta_comparison.py
+++ fasta_generator/src/fasta_comparison.py
@@ -2,6 +2,7 @@ import subprocess
 import argparse
 import os
 import pandas as pd
+from Bio import SeqIO
 
 
 def fasta_maker(dnt_list, output, name_file):
@@ -17,7 +18,7 @@ def fasta_maker(dnt_list, output, name_file):
                            dnt_list["CC"], "--CG", "--CT", dnt_list["CT"], "--GA", dnt_list["GA"], "--GC",
                            dnt_list["GC"], "--GG", dnt_list["GG"], "--GT", dnt_list["GT"], "--TA", dnt_list["TA"],
                            "--TC", dnt_list["TC"], "--TG", dnt_list["TG"],
-                           "--output", output, "--filename", name_file],
+                           "--output", output, "--filename", name_file, "--size_max", "270"],
                           stderr=subprocess.STDOUT)
 
 
@@ -53,6 +54,66 @@ def fasta_creator(input_xls, bg_xls, output):
     fasta_maker(dnt_list, output, "input")
     dnt_list = proportion_getter(bg_xls)
     fasta_maker(dnt_list, output, "background")
+
+
+def calcul_dic(dic, seq, length, count):
+    """
+    :param dic: (a dictionary of float) freq of the word af interest
+    :param seq: (string) the nt sequence
+    :param length: (int) the length of the word that we want to find
+    :param count: (int) the number of words in the dic
+    :return: 'dic' completed
+    """
+    for i in range(len(seq)-(length-1)):
+        if seq[i:i+length] not in dic.keys():
+            dic[seq[i:i+length]] = 1
+            count += 1
+        else:
+            dic[seq[i:i+length]] += 1
+            count += 1
+    return dic, count
+
+def get_frequencies(dic, count):
+    """
+    :param dic: (dictionary of int) each key correspond to an oligo-mer and it's value : the number of time it was
+    seen in the dictionary
+    :param count: (int) the total number of word in that dictionary
+    :return: (dic of float) the frequencies of each word
+    """
+    for key in dic.keys():
+        dic[key] = float(dic[key]) / count
+    return dic
+
+def counting_letters_frequencies(fasta_file):
+    """
+    :param fasta_file: (string)  a fasta file
+    :return: (5 dic of float):
+         - dic_6l : link each word of 6 letters found in the fasta files with its frequencies in the fasta sequence
+         - dic_5l : link each word of 5 letters found in the fasta files with its frequencies in the fasta sequence
+         - dic_4l : link each word of 4 letters found in the fasta files with its frequencies in the fasta sequence
+         - dic_3l : link each word of 3 letters found in the fasta files with its frequencies in the fasta sequence
+    """
+    dic_6l = {}
+    count_6 = 0
+    dic_5l = {}
+    count_5 = 0
+    dic_4l = {}
+    count_4 = 0
+    dic_3l = {}
+    count_3 = 0
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        dic_6l, count_6 = calcul_dic(dic_6l, record.seq, 6, count_6)
+        dic_5l, count_5 = calcul_dic(dic_5l, record.seq, 6, count_5)
+        dic_4l, count_4 = calcul_dic(dic_4l, record.seq, 6, count_4)
+        dic_3l, count_3 = calcul_dic(dic_3l, record.seq, 6, count_3)
+
+    dic_6l = get_frequencies(dic_6l, count_6)
+    dic_5l = get_frequencies(dic_5l, count_5)
+    dic_4l = get_frequencies(dic_4l, count_4)
+    dic_3l = get_frequencies(dic_3l, count_3)
+
+    return dic_6l, dic_5l, dic_4l, dic_3l
+
 
 
 
