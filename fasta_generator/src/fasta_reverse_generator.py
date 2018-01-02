929029a06fc2d6ce53c32a0e240ae0d6e3146466
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-01-02 16:10:08 +0100

    src/fasta_reverse_generator.py creation of a file to generate nucleotide sequence with a given feature frequency

---
+++ fasta_generator/src/fasta_reverse_generator.py
@@ -0,0 +1,10 @@
+"""Description: script that can generate a fasta file with random sequences.
+It will use a given probability to have a certain protein feature at each position of
+an amino acid sequence."""
+
+
+#############################
+#          IMPORTS
+#############################
+
+import argparse
