e2d4b3335a0078e7fcfed3c547ec881ea1dd3c40
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-01-07 17:16:26 +0100

    src/middle_bed_launcher.py : launch src/middle_bed_enrichment.py many times

---
+++ clip_analysis/src/middle_bed_launcher.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+"""
+Description:
+    This script will launch ``middle_bed_enrichment`` for every bed in a given folder
+"""
+
+
+import os
+import subprocess
+import argparse
+
+
+def main(trna_launcher, folder_bed, fasterdb_bed, output):
+    """
+
+    :param trna_launcher: (string) file corresponding to the tRNA launcher
+    :param folder_bed: (string) folder containing the bed files
+    :param output:  (string) path where the output will be created
+    """
+    cur_folder = os.path.realpath(os.path.dirname(__file__))
+    bed_files = sorted(os.listdir(folder_bed))
+    bed_files = [folder_bed + my_file for my_file in bed_files]
+    for my_bed in bed_files:
+        print("Working on %s" % my_bed)
+        name_file = os.path.basename(my_bed).split(".")[0]
+        output_folder = output + name_file
+        if not os.path.isdir(output_folder):
+            os.mkdir(output_folder)
+
+        cmd = "python3 %s/middle_bed_enrichment.py  --output %s --name %s --clip_bed %s  --fasterdb_bed %s \
+        --trna_launcher %s" % (cur_folder, output_folder, name_file, my_bed, fasterdb_bed, trna_launcher)
+        print(cmd)
+        subprocess.check_call(cmd, shell=True, stderr=subprocess.STDOUT)
+
+
+def launcher():
+    """
+    function that contains a parser to launch the program
+    """
+    # description on how to use the program
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="""
+    Launch the script ``middle_bed_enrichment`` multiple time
+
+    """)
+    # Arguments for the parser
+    required_args = parser.add_argument_group("Required argument")
+
+
+    parser.add_argument('--output', dest='output',
+                        help="""path where the result will be created - default : current directory""",
+                        default=".")
+    required_args.add_argument('--clip_folder', dest='clip_folder',
+                               help="The bed folder containing bed file",
+                               required=True)
+
+    required_args.add_argument('--fasterdb_bed', dest='fasterdb_bed',
+                               help="""the bed containing all fasterDB exons""",
+                               required=True)
+    required_args.add_argument('--trna_launcher', dest='trna_launcher',
+                               help="""file corresponding the tRNA launcher""",
+                               required=True)
+    args = parser.parse_args()  # parsing arguments
+
+    # Defining global parameters
+    if args.output[-1] != "/":
+        args.output += "/"
+    main(args.trna_launcher, args.clip_folder, args.fasterdb_bed, args.output)
+
+
+if __name__ == "__main__":
+    launcher()
+
